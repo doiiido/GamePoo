5382307a8f3df1062dd5b8ce4331dcdcafdb45a2
EdgarFabiano <eddy.led@gmail.com>
2015-12-01 01:36:10 -0200

    Adição do parâmetro speedY no construtor do ChasingYEnemy na Wave2

--- Game2d/src/unb/cic/poo/game2d/fases/Fase1.java
+++ Game2d/src/unb/cic/poo/game2d/fases/Fase1.java
@@ -1,10 +1,11 @@
 package unb.cic.poo.game2d.fases;
 
 import unb.cic.poo.game2d.waves.Wave1;
+import unb.cic.poo.game2d.waves.Wave2;
 
 public class Fase1 extends Fase{
 	public Fase1(){
 		this.waves.add(new Wave1());
-		this.waves.add(new Wave1());
+		this.waves.add(new Wave2());
 	}	
 }
--- Game2d/src/unb/cic/poo/game2d/waves/Wave2.java
+++ Game2d/src/unb/cic/poo/game2d/waves/Wave2.java
@@ -4,16 +4,23 @@ import java.util.ArrayList;
 
 import unb.cic.poo.game2d.enemies.*;
 import unb.cic.poo.game2d.GameActivity;
+import unb.cic.poo.game2d.GameManager;
 import unb.cic.poo.game2d.waves.Wave;
 
-public class Wave1 extends Wave {
+public class Wave2 extends Wave {
 
-	public Wave1(){
+	public Wave2(){
 		super();
 		this.enemies = new ArrayList<Enemy>();
-		this.enemies.add(new CommonEnemy(GameActivity.CAMERA_WIDTH, (float) (GameActivity.CAMERA_HEIGHT*0.5)));
+		//this.enemies.add(new VerticalMovementEnemy(GameActivity.CAMERA_WIDTH, (float) (GameActivity.CAMERA_HEIGHT*0.5),  GameActivity.CAMERA_WIDTH*0.9f));
 		this.enemies.add(new CommonEnemy(GameActivity.CAMERA_WIDTH+200, (float) (GameActivity.CAMERA_HEIGHT*0.8)));
 		this.enemies.add(new FreezedShootingEnemy(GameActivity.CAMERA_WIDTH+200,(float) (GameActivity.CAMERA_HEIGHT*0.2), GameActivity.CAMERA_WIDTH*0.9f));
-	
+		this.enemies.add(new ChasingYEnemy(GameActivity.CAMERA_WIDTH+50, (float) (GameActivity.CAMERA_HEIGHT*0.2), GameActivity.CAMERA_WIDTH*0.9f, GameManager.getInstance().getPlayer().getSpeed()*0.1f));
+
+
+		
 	}
 }
+
+
+
