94638a037d44520abe46b8bf3a8680dc456d74e6
Fabio <fabio.cmarques@hotmail.com>
2015-12-09 02:54:09 -0200

    Adicionadas waves 2 e 3 da fase 2.

---
+++ Game2d/src/unb/cic/poo/game2d/waves/Fase2/Wave2.java
@@ -0,0 +1,24 @@
+package unb.cic.poo.game2d.waves.Fase2;
+
+import java.util.ArrayList;
+
+import unb.cic.poo.game2d.GameActivity;
+import unb.cic.poo.game2d.enemies.*;
+import unb.cic.poo.game2d.waves.Wave;
+
+public class Wave2 extends Wave{
+	
+	public Wave2(){
+		super();
+		this.enemies = new ArrayList<Enemy>();
+		
+		enemies.add(new ChasingYEnemy(GameActivity.CAMERA_WIDTH+100, GameActivity.CAMERA_HEIGHT*0.3f, GameActivity.CAMERA_WIDTH*0.3f, 250));
+		enemies.add(new ChasingYEnemy(GameActivity.CAMERA_WIDTH+100, GameActivity.CAMERA_HEIGHT*0.5f, GameActivity.CAMERA_WIDTH*0.5f, 340));
+		enemies.add(new ChasingYEnemy(GameActivity.CAMERA_WIDTH+100, GameActivity.CAMERA_HEIGHT*0.7f, GameActivity.CAMERA_WIDTH*0.7f, 300));
+		enemies.add(new ChasingYEnemy(GameActivity.CAMERA_WIDTH+100, GameActivity.CAMERA_HEIGHT*0.6f, GameActivity.CAMERA_WIDTH*0.9f, 420));
+		enemies.add(new VerticalMovementLaser(GameActivity.CAMERA_WIDTH+200f, GameActivity.CAMERA_HEIGHT*0.4f, GameActivity.CAMERA_WIDTH*0.8f, 0.8f));
+		enemies.add(new VerticalMovementLaser(GameActivity.CAMERA_WIDTH+150f, GameActivity.CAMERA_HEIGHT*0.9f, GameActivity.CAMERA_WIDTH*0.8f, 0.6f));
+		
+	}
+
+}
---
+++ Game2d/src/unb/cic/poo/game2d/waves/Fase2/Wave3.java
@@ -0,0 +1,32 @@
+package unb.cic.poo.game2d.waves.Fase2;
+
+import java.util.ArrayList;
+
+import unb.cic.poo.game2d.GameActivity;
+import unb.cic.poo.game2d.enemies.*;
+import unb.cic.poo.game2d.waves.Wave;
+
+public class Wave3 extends Wave{
+	public Wave3(){
+		super();
+		this.enemies = new ArrayList<Enemy>();
+		
+		enemies.add(new FreezedShootingEnemy(GameActivity.CAMERA_WIDTH + 100f, GameActivity.CAMERA_HEIGHT*0.1f, GameActivity.CAMERA_WIDTH*0.5f));
+		enemies.add(new FreezedShootingEnemy(GameActivity.CAMERA_WIDTH + 100f, GameActivity.CAMERA_HEIGHT*0.3f, GameActivity.CAMERA_WIDTH*0.5f));
+		enemies.add(new FreezedShootingEnemy(GameActivity.CAMERA_WIDTH + 100f, GameActivity.CAMERA_HEIGHT*0.5f, GameActivity.CAMERA_WIDTH*0.5f));
+		enemies.add(new FreezedShootingEnemy(GameActivity.CAMERA_WIDTH + 100f, GameActivity.CAMERA_HEIGHT*0.7f, GameActivity.CAMERA_WIDTH*0.5f));
+		enemies.add(new FreezedShootingEnemy(GameActivity.CAMERA_WIDTH + 100f, GameActivity.CAMERA_HEIGHT*0.9f, GameActivity.CAMERA_WIDTH*0.5f));
+		
+		enemies.add(new FreezedShootingEnemy(GameActivity.CAMERA_WIDTH + 200f, GameActivity.CAMERA_HEIGHT*0.2f, GameActivity.CAMERA_WIDTH*0.7f));
+		enemies.add(new FreezedShootingEnemy(GameActivity.CAMERA_WIDTH + 200f, GameActivity.CAMERA_HEIGHT*0.4f, GameActivity.CAMERA_WIDTH*0.7f));
+		enemies.add(new FreezedShootingEnemy(GameActivity.CAMERA_WIDTH + 200f, GameActivity.CAMERA_HEIGHT*0.6f, GameActivity.CAMERA_WIDTH*0.7f));
+		enemies.add(new FreezedShootingEnemy(GameActivity.CAMERA_WIDTH + 200f, GameActivity.CAMERA_HEIGHT*0.8f, GameActivity.CAMERA_WIDTH*0.7f));
+		
+		enemies.add(new FreezedShootingEnemy(GameActivity.CAMERA_WIDTH + 300f, GameActivity.CAMERA_HEIGHT*0.1f, GameActivity.CAMERA_WIDTH*0.9f));
+		enemies.add(new FreezedShootingEnemy(GameActivity.CAMERA_WIDTH + 300f, GameActivity.CAMERA_HEIGHT*0.3f, GameActivity.CAMERA_WIDTH*0.9f));
+		enemies.add(new FreezedShootingEnemy(GameActivity.CAMERA_WIDTH + 300f, GameActivity.CAMERA_HEIGHT*0.5f, GameActivity.CAMERA_WIDTH*0.9f));
+		enemies.add(new FreezedShootingEnemy(GameActivity.CAMERA_WIDTH + 300f, GameActivity.CAMERA_HEIGHT*0.7f, GameActivity.CAMERA_WIDTH*0.9f));
+		enemies.add(new FreezedShootingEnemy(GameActivity.CAMERA_WIDTH + 300f, GameActivity.CAMERA_HEIGHT*0.9f, GameActivity.CAMERA_WIDTH*0.9f));
+		
+	}
+}
